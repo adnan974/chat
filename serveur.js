1a6ea7c56f7b3e228df07cc0874e4c37f9b58226
adn974 <56817528+adnan974@users.noreply.github.com>
2020-08-22 16:58:57 +0400

    ajout du module socket io

---
+++ serveur.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+// Question: pourquoi je met "app" en parametre. cad comment fonctionne la methode createServer ?
+var server = require("http").createServer(app);
+
+app.get('/',function(req,res){
+    res.render('chat.ejs');
+})
+app.post("/send",function(req,res){
+    res.render(req.body['chatEntry']);
+})
+
+var io = require("socket.io").listen(server);
+
+io.sockets.on("connection",function(socket){
+    console.log("un client s'est connecté");
+    socket.emit("chatEntry","hello");
+})
+
+//app.listen(8080);
+server.listen(8080);
